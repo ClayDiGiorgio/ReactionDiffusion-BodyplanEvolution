acaa69a763aae17c1012d71cff0f4382c897777c
Clay DiGiorgio <cldigiorgio@knights.ucf.edu>
2020-07-25 12:12:42 -0400

    undid all diploid changes, the code works again and I'm going to retry adding this feature

--- main_gridBased.py
+++ main_gridBased.py
@@ -1,15 +1,3 @@
-###
-# TODO List
-###
-# - add a field in the chromosome saying which index of 
-#   morphogen it codes for.
-#       * Right now, a haploid genome always codes for morphogens 0 to N-1
-#         they can't skip morphogen 2, for example
-# - add some way to linearize each chromosome
-# - support reproduction between two diploid genomes, including crossover
-#   and chromosome duplication / deletion
-
-
 #from benmaier_reactionDiffusion.gray_scott_static import draw
 from benmaier_reactionDiffusion.gray_scott_static import get_initial_A_and_B
 from benmaier_reactionDiffusion.gray_scott_static import update
@@ -121,51 +109,12 @@ def randomChromosome(N):
     return chromosome
 
 
-# returns a diploid genome, eg
-#   ( 
-#    [                              # +-----< haploid genome 1 (genomeF)
-#     {                           \ # | +--< Chromosome 1
-#       "f": {"-":0.060},         \ # | |
-#       "k": {"-":0.062},         \ # | |
-#       "DA":{"-":0.16},          \ # | |
-#       "DB":{"-":0.08}           \ # | |
-#     }                             # | +-->
-#    ],                             # +----->
-#
-#    [                              # +-----< haploid genome 2 (genomeM)
-#     {                           \ # | +--< Chromosome 1
-#       "f": {"-":0.060},         \ # | |
-#       "k": {"-":0.062},         \ # | |
-#       "DA":{"-":0.16},          \ # | |
-#       "DB":{"-":0.08}           \ # | |
-#     },                            # | +-->
-#     {                           \ # | +--< Chromosome 2
-#       "f": {"-":0.060},         \ # | |
-#       "k": {"-":0.062},         \ # | |
-#       "DA":{"-":0.16},          \ # | |
-#       "DB":{"-":0.08}           \ # | |
-#     }                             # | +-->
-#    ]                              # +----->
-#   )
 def randomGenome(N):
-    haploid1 = [defaultChromosome()]
-    for i in range(N-1):
-        haploid1.append(randomChromosome(N))
-        
-    haploid2 = [defaultChromosome()]
-    for i in range(N-1):
-        haploid2.append(randomChromosome(N))
-    
-    genome = (haploid1, haploid2)
+    genome = [defaultChromosome()]
+    for i in range(N):
+        genome.append(randomChromosome(N))
     return genome
 
-
-def simpleDefaultGenome(N):
-    haploid1 = [defaultChromosome() for i in range(N)]
-    haploid2 = [defaultChromosome() for i in range(N)]
-    
-    genome = (haploid1, haploid2)
-    return genome
 
 # if a key is "-" then it codes for just a scalar, not a dependancy coefficient
 def decode(gene, morphogens): 
@@ -180,76 +129,33 @@ def decode(gene, morphogens):
     return result
 
 
-# genomeF and genomeM are haploid genomes (they only contain at most one of each chromosome)  
-def simulate(genomeF, genomeM, gridSize=200, N_simulation_steps=1000, delta_t=1.0):
-    N = gridSize
-    #morphogens = [np.asarray(get_initial_A_and_B(N)) for i in range(max(len(genomeF), len(genomeM)))]
-    # morphogens is of the form [np.array([][], [][])]
-    numGenomes = max(len(genomeF), len(genomeM))
-    morphogens = [tuple(np.asarray(L) for L in get_initial_A_and_B(N)) for i in range(numGenomes)]
-    # morphogens is of the form [(np.array(2d), np.array(2d)), ...]
+def __simulationStep(genome, morphogens, delta_t):
+    newMorphogens = []
+    # calculate parameters (important for dependancies)
+    fValues = []
+    kValues = []
+    DAValues = []
+    DBValues = []
+    for chromosome in genome:
+        fValues.append(decode(chromosome["f"], morphogens))
+        kValues.append(decode(chromosome["k"], morphogens))
+        DAValues.append(decode(chromosome["DA"], morphogens))
+        DBValues.append(decode(chromosome["DB"], morphogens))
     
-    #print(morphogens)
-    #print("=============")
-    #print(morphogens[0])
-    #print("=============")
-    #print(morphogens[0][0].shape)
-    #print(numGenomes)
+    
+    for morphogen, f, k, DA, DB in zip(morphogens, fValues, kValues, DAValues, DBValues):
+        newMorphogens.append(tuple(update(*morphogen, DA, DB, f, k, delta_t)))
+    
+    return newMorphogens
+    
+
+  
+def simulate(genome, gridSize=200, N_simulation_steps=1000, delta_t=1.0):
+    N = gridSize
+    morphogens = [np.asarray(get_initial_A_and_B(N)) for i in range(len(genome))]
     
     for step in range(N_simulation_steps):
-        newMorphogens = []
-        
-        #print("\n===== Step " + str(step) + " ======")
-        for haploid in (genomeF, genomeM):
-            thisHaploidNewMorphogens = []
-            # calculate parameters (important for calculating dependancies properly)
-            fValues = []
-            kValues = []
-            DAValues = []
-            DBValues = []
-            for chromosomeF, chromosomeM in zip(genomeF, genomeM):
-                fValues. append(decode(chromosomeF["f"],  morphogens) + decode(chromosomeM["f"],  morphogens))
-                kValues. append(decode(chromosomeF["k"],  morphogens) + decode(chromosomeM["k"],  morphogens))
-                DAValues.append(decode(chromosomeF["DA"], morphogens) + decode(chromosomeM["DA"], morphogens))
-                DBValues.append(decode(chromosomeF["DB"], morphogens) + decode(chromosomeM["DB"], morphogens))
-            
-            #i = 0
-            for morphogen, f, k, DA, DB in zip(morphogens, fValues, kValues, DAValues, DBValues):
-                #print("\nChromosome/morphogen " + str(i))
-                #i += 1
-                
-                A = morphogen[0]
-                B = morphogen[1]
-                f = np.clip(f, 0, 1)
-                k = np.clip(k, 0, 1)
-                DA = np.clip(DA, 0, 1)
-                DB = np.clip(DB, 0, 1)
-                
-                #print("A = array" + str(A.shape))
-                #print("B = array" + str(A.shape))
-                #if type(f) == type(A):
-                    #print("f = array" + str(f.shape))
-                #if type(k) == type(A):
-                    #print("k = array" + str(k.shape))
-                #if type(DA) == type(A):
-                    #print("DA = array" + str(DA.shape))
-                #if type(DB) == type(A):
-                    #print("DB = array" + str(DB.shape))
-                
-                A,B = update(A, B, DA, DB, f, k, delta_t)
-                #print("\t" + str(A.shape))
-                thisHaploidNewMorphogens.append((A, B))
-                
-            newMorphogens.append(thisHaploidNewMorphogens)
-        
-        # newMorphogens is of the form [[(A,B), ...], [(A, B), ...]]
-        # where newMorphogens[0] is a list of all updated morphogen A, B pairs
-        # updated by genomeF
-        # and newMorphogens[1] is a list of all updated morphogens by genomeM
-        
-        # morphogens = [(0.5*newFPair[0]+0.5*newMPair[0], 0.5*newFPair[1] + 0.5*newMPair[1]) for newFPair, newMPair in zip(newMorphogens[0], newMorphogens[1])]
-        morphogens = newMorphogens[0]
-        print(morphogens)
+        morphogens = __simulationStep(genome, morphogens, delta_t)
             
     draw( morphogens )
 
@@ -265,12 +171,9 @@ if __name__=="__main__":
         #defaultDependancyGeneSet() \
     #]
     #simulate(genome)
+    import pprint
+    pp = pprint.PrettyPrinter(indent=4)
     
-    simulate(*simpleDefaultGenome(4))
-    
-    #import pprint
-    #pp = pprint.PrettyPrinter(indent=4)
-    
-    #genome = randomGenome(4)
-    #pp.pprint(genome)
-    #simulate(*genome)
+    genome = randomGenome(4)
+    pp.pprint(genome)
+    simulate(genome)
