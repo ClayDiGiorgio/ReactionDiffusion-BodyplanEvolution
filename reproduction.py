def4851597bf90f9a548e5279bf6c8362db385d5
Clay DiGiorgio <cldigiorgio@knights.ucf.edu>
2020-07-27 11:12:27 -0400

    file renaming, removed unneeded files

---
+++ reproduction.py
@@ -0,0 +1,10 @@
+
+# should be of the form /(\{+\-0.{0-9}+\}+)+}/ 
+def chromosomeFromLinearization(chromoString):
+    chromosome = {}
+    genes = ["f", "k", "DA", "DB"]
+    
+    for i in range(len(chromoString)):
+        
+
+
