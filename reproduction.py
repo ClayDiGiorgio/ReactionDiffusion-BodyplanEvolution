933b89fc409067d3eab50a159b9a01523e64fd9b
Clay DiGiorgio <cldigiorgio@knights.ucf.edu>
2020-07-27 11:25:14 -0400

    started reproduction, completed function for constructing a chromosome for computation from a linearized gene for reproduction

--- reproduction.py
+++ reproduction.py
@@ -1,10 +1,43 @@
 
-# should be of the form /(\{+\-0.{0-9}+\}+)+}/ 
-def chromosomeFromLinearization(chromoString):
+# should be a list of elements, where each element is one of the following
+# }
+# {
+# ~
+# {A|B}{0-9}+
+# a floating point number
+#
+
+def chromosomeFromLinearization(linearChromo):
     chromosome = {}
     genes = ["f", "k", "DA", "DB"]
     
-    for i in range(len(chromoString)):
+    bracketsOpen = False
+    currentGene = {}
+    i = -1
+    for _ in range(len(linearChromo)):
+        i += 1
         
+        if not bracketsOpen:
+            if linearChromo[i] == '{':
+                bracketsOpen = True
+            continue
+        
+        if linearChromo[i] == '}':
+            bracketsOpen = False
+            chromosome[genes.pop(0)] = currentGene
+            currentGene = {}
+            continue
+        
+        if linearChromo[i] == '~':
+            if i == len(linearChromo)-1:
+                continue
+            i += 1
+            currentGene['~'] = linearChromo[i]
+            continue
 
-
+        # the only case left is A0
+        if type(linearChromo[i]) == type('string'): 
+            if i == len(linearChromo)-1:
+                continue
+            currentGene[linearChromo[i]] = linearChromo[i+1]
+            i += 1
